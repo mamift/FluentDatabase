00f16212d03bec6174aba0bde7f26bbcd4cbb307
Muhammad Miftah <muhammad.m@mqf.net.au>
2021-05-26 09:31:37 +1000

    Added unit testing project based on nunit 3.

---
+++ src/FluentDb.Tests/DatabaseTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using NUnit.Framework;
+
+namespace FluentDb.Tests
+{
+	[TestFixture]
+	public class DatabaseTest
+	{
+		[Test]
+		public void Test()
+		{
+			foreach (DatabaseType databaseType in Enum.GetValues(typeof(DatabaseType))) {
+				var fileName = $"{Environment.CurrentDirectory}\\{databaseType}.sql";
+				using (var stream = File.OpenWrite(fileName))
+				using (var writer = new StreamWriter(stream)) {
+					DatabaseFactory.Create(databaseType)
+						.WithName("Business")
+						.UsingSchema("Test")
+						.HasTable(
+							table => table
+								.WithName("Companies")
+								.HasColumn(
+									column => column.WithName("Id").OfType(SqlDbType.Int).IsAutoIncrementing()
+										.HasConstraint(constraint => constraint.OfType(ConstraintType.NotNull))
+										.HasConstraint(
+											constraint =>
+												constraint.OfType(ConstraintType.PrimaryKey)
+													.WithName("PK_Companies_Id"))
+								)
+								.HasColumn(
+									column => column.WithName("Name").OfType(SqlDbType.NVarChar, 100)
+										.HasConstraint(constraint => constraint.OfType(ConstraintType.NotNull))
+								)
+						)
+						.HasTable(
+							table => table
+								.WithName("Employees")
+								.HasColumn(
+									column => column.WithName("Id").OfType(SqlDbType.Int).IsAutoIncrementing()
+										.HasConstraint(constraint => constraint.OfType(ConstraintType.NotNull))
+										.HasConstraint(
+											constraint =>
+												constraint.OfType(ConstraintType.PrimaryKey)
+													.WithName("PK_Employees_Id"))
+								)
+								.HasColumn(
+									column => column.WithName("CompanyId").OfType(SqlDbType.Int)
+										.HasConstraint(constraint => constraint.OfType(ConstraintType.NotNull))
+										.HasConstraint(
+											constraint =>
+												constraint.WithName("FK_Employees_CompanyId")
+													.OfType(ConstraintType.ForeignKey)
+													.HasReferenceTo("Companies", "Id"))
+								)
+								.HasColumn(
+									column => column.WithName("Name").OfType(SqlDbType.NVarChar, 50)
+										.HasConstraint(constraint => constraint.OfType(ConstraintType.NotNull))
+								)
+								.HasColumn(
+									column => column.WithName("Bio").OfType(SqlDbType.NVarChar, ColumnSize.Max)
+								)
+						).Write(writer);
+				}
+			}
+		}
+	}
+}
