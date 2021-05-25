ccd7311a3dcae8e0bd113ed1440a0eeabb0664bd
Muhammad Miftah <muhammad.m@mqf.net.au>
2021-05-26 08:59:22 +1000

    Removed all #region directives.

--- src/FluentDb/Sqlite/FluentDatabaseSqliteException.cs
+++ src/FluentDb/Sqlite/FluentDatabaseSqliteException.cs
@@ -1,8 +1,7 @@
-﻿#region License
-// Copyright 2009 Josh Close
+﻿// Copyright 2009 Josh Close
 // This file is a part of FluentDatabase and is licensed under the MS-PL
 // See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html
-#endregion
+
 namespace FluentDb.Sqlite
 {
 	/// <summary>
